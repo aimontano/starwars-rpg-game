54c0f363a3486b6b79184fbe8fd60c955ac70f1a
aimontano <aimorales.montano@gmail.com>
2018-08-15 23:06:44 -0400

    Finished styles

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,4 @@
+// boolean - character has not bee
 let hasCharacter = false;
 
 
@@ -6,9 +7,13 @@ $(document).ready(function(){
 	$('#characters div').click(function(){
 		if(!hasCharacter){
 			hasCharacter = true;
+
 			$(this).remove();
 			$('#selectedCharacter').append(this);
+
 			$('#enemiesAvailable').append($('#characters div'));
+			$('#enemiesAvailable div').css('background', 'red');
+			$('#enemiesAvailable div').css('color', 'white');
 		}
 	});
 
